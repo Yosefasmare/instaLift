bfa705e7e89ada9146f65c91bad6dd158db71909
Yosefasmare <josephasmare953@gmail.com>
2025-04-10 09:10:36 +0300

    Initial commit from Create Next App

---
+++ lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_HSOz-BfRJ2c-ilb7hkluKyS15zV5hFQ",
+  authDomain: "instafollo.firebaseapp.com",
+  projectId: "instafollo",
+  storageBucket: "instafollo.firebasestorage.app",
+  messagingSenderId: "607054849343",
+  appId: "1:607054849343:web:1eaefc21a48961209e9f8a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
